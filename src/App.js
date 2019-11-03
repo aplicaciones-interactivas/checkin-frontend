3a8d12cfabe9b082d3e032b253e52ba13829da29
eduardo.hernandez <hernandezed.1991@gmail.com>
2019-11-03 04:14:19 -0300

    rewrite home as react bootstrap component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ImgMediaCard from './components/navMain';
 import IntegrationReactSelect from './components/searchCountry';
 import MaterialUIPickers from './components/datePicker';
 import InputField from './components/inputPeople';
-import Buttons from  './components/primaryButton';
+import Buttons from './components/primaryButton';
 import TransitionsModal from './components/modal';
 import FormControlLabelPosition from './components/checkBox';
 import {BrowserRouter as Router} from 'react-router-dom';
@@ -14,117 +14,87 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import HotelInfo from './components/hotelInfo';
 import ButtonAppBar from './components/appBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const results = [
-    {
-    nombre: "Sheraton",
-    precio: "150",
-    ubicacion: "Buenos Aires",
-    estrellas: "5"
-  },
-  {
-    nombre: "Hilton",
-    precio: "100",
-    ubicacion: "Rosario",
-    estrellas: "5"
-  },
-  {
-    nombre: "Milton",
-    precio: "100",
-    ubicacion: "Cordoba",
-    estrellas: "3"
-  },
-  {
-    nombre: "Los Andes Apart",
-    precio: "100",
-    ubicacion: "Misiones",
-    estrellas: "3"
-  },
-  {
-    nombre: "Pestilloso",
-    precio: "100",
-    ubicacion: "Jujuy",
-    estrellas: "1"
-  },
-  {
-    nombre: "Howard Jhonson",
-    precio: "100",
-    ubicacion: "Buenos Aires",
-    estrellas: "5"
-  },
-]
+    const results = [
+        {
+            nombre: "Sheraton",
+            precio: "150",
+            ubicacion: "Buenos Aires",
+            estrellas: "5"
+        },
+        {
+            nombre: "Hilton",
+            precio: "100",
+            ubicacion: "Rosario",
+            estrellas: "5"
+        },
+        {
+            nombre: "Milton",
+            precio: "100",
+            ubicacion: "Cordoba",
+            estrellas: "3"
+        },
+        {
+            nombre: "Los Andes Apart",
+            precio: "100",
+            ubicacion: "Misiones",
+            estrellas: "3"
+        },
+        {
+            nombre: "Pestilloso",
+            precio: "100",
+            ubicacion: "Jujuy",
+            estrellas: "1"
+        },
+        {
+            nombre: "Howard Jhonson",
+            precio: "100",
+            ubicacion: "Buenos Aires",
+            estrellas: "5"
+        },
+    ]
 
 //const results = fetch('localhost:3200/Hoteles')
 
 
-const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-const handleOpen = () => {
-  setOpen(true);
-};
+    const handleOpen = () => {
+        setOpen(true);
+    };
 
-const handleClose = () => {
-  setOpen(false);
-};
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-const HomePage = () => {
-return <Home desde={<MaterialUIPickers label="Desde"/>} hasta={<MaterialUIPickers label="Hasta"/>}/>;
-};
+    const HomePage = () => {
+        return <Home/>;
+    };
 
-const SigningIn = () => {
-  return <SignIn/>;
-};
+    const SigningIn = () => {
+        return <SignIn/>;
+    };
 
-const SigningUp = () => {
-  return <SignUp/>;
-};
+    const SigningUp = () => {
+        return <SignUp/>;
+    };
 
-const MoreInfo = (props) => {
-  return <HotelInfo p={props.location.hotelInfo}/>;
-};
+    const MoreInfo = (props) => {
+        return <HotelInfo p={props.location.hotelInfo}/>;
+    };
 
-  return (
-    <Router>
-  <div class="principal">
-  <Route path="/InfoHotel" exact strict component={MoreInfo}/>
-  <Route path="/" exact strict component={HomePage}/>
-  <Route path="/SignIn" exact strict component={SigningIn}/>
-  <Route path="/SignUp" exact strict component={SigningUp}/>
-  <div class="home">
-  </div>
-  <Route path="/Hoteles" exact strict render={
-    () => {return (
-  <div class="Busqueda">
-    <ButtonAppBar/>
-  <div class="hoteles">
-    <div class="contenedor-hoteles">
-      {results.map(hotel => <div class="hotel"><ImgMediaCard hotel={hotel} handleOpen={handleOpen}/></div>)}
-    </div>
-    <div class="contenedor-filtros">
-        <form>
-          <IntegrationReactSelect/>
-          <MaterialUIPickers label="Desde"/>
-          <MaterialUIPickers label="Hasta"/>
-          <InputField/>
-          <Buttons id="formButton"/>
-        {/*fetch(onclick)*/}
-        </form>
-        <div class="contenedor-otros_filtros">
-            <FormControlLabelPosition nameLabel="1 estrella"/>
-            <FormControlLabelPosition nameLabel="2 estrellas"/>
-            <FormControlLabelPosition nameLabel="3 estrellas"/>
-            <FormControlLabelPosition nameLabel="4 estrellas"/>
-            <FormControlLabelPosition nameLabel="5 estrellas"/>
-        </div>
-    </div>
-  </div>
-  </div>
-  );}}/>
-  <TransitionsModal open={open} handleClose={handleClose}/>
-  </div>
-  </Router>
-  );
+    return (
+        <Router>
+            <div class="principal">
+                <Route path="/InfoHotel" exact strict component={MoreInfo}/>
+                <Route path="/" exact strict component={HomePage}/>
+                <Route path="/SignIn" exact strict component={SigningIn}/>
+                <Route path="/SignUp" exact strict component={SigningUp}/>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,77 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../index.css';
 import {Link} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import {Button, Col, Container, Row} from "react-bootstrap";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 
-export default function Home ({desde, hasta}){
-    return( 
-      <div class="header">
-           <head>
-                <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet"/>
-                <meta name="viewport" content="width=device-width, user-scalable=no"/>
-                <link rel="apple-touch-icon" href="logo192.png" />
-                <link rel="stylesheet" href="../src/index.css"></link>
-            </head>
-            <div class="menu">
-                <div class="contenedor">
-                <p class="logo">Check-In</p>
-                <img class="menu-icon" src="../images/menu.png" alt=""/>
-                <nav>
-                    <ul class="lista-menu">
-                    <li><Link to="/SignIn">Login</Link></li> 
-                    <li><Link to="/SignUp">SignUp</Link></li>
-                    </ul>
-                </nav>
+export default class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            from: new Date().toISOString(),
+            until: new Date().toISOString(),
+        };
+    }
+
+    render() {
+        return (
+            <Container fluid={true} className={"header"}>
+                <Navbar variant="dark">
+                    <Navbar.Brand href="/">Check-In</Navbar.Brand>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav>
+                            <Nav.Link href="/SignIn">Sign in</Nav.Link>
+                            <Nav.Link href="/SignUp">Sign up</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <Row className='mt-5'>
+                    <Col xs={12}>
+                        <h1 className="text-center white">Check-In</h1>
+                        <h4 className="text-center white">Find your ideal hotel worldwide at the best price</h4>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <InputGroup className="mb-3">
+                            <FormControl
+                                placeholder="Where are you go?"
+                                aria-label="where-u-go"
+                            />
+                            <InputGroup.Prepend>
+                                <InputGroup.Text>From</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <FormControl type={'date'}></FormControl>
+                            <InputGroup.Prepend>
+                                <InputGroup.Text>To</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <FormControl type={'date'}></FormControl>
+                            <InputGroup.Prepend>
+                                <InputGroup.Text>Persons</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <FormControl type={'number'}></FormControl>
+                            <Button href={"/Hoteles"}>Search</Button>
+                        </InputGroup>
+
+                    </Col>
+                </Row>
+            </Container>
+            /*<div class="contenedor" id="contenedor-titulo-flex">
+                <div class="contenedor-titulo">
+                    <h1>Check-In</h1>
+                    <h2>Hacé Check-In en los mejores hoteles</h2>
+                    <input type="text" placeholder="¿A dónde vas?" id="selector-destino"/>
+                    <input type="date" id="selector-fecha"/>
+                    <input type="date" id="selector-fecha"/>
+                    <input type="number" id="selector-personas"/>
+                    <Link to="/Hoteles">Buscar</Link>
                 </div>
-            </div>
-            <div class="contenedor" id="contenedor-titulo-flex">
-            <div class="contenedor-titulo">
-                <h1>Check-In</h1>
-                <h2>Hacé Check-In en los mejores hoteles</h2>
-                <input type="text" placeholder="¿A dónde vas?" id="selector-destino"/>
-                <input type="date" id="selector-fecha"/>
-                <input type="date" id="selector-fecha"/>
-                <input type="number" id="selector-personas"/>
-                <Link to="/Hoteles">Buscar</Link> {/*Query To localhost:3200/HotelesConFiltro*/}
-            </div>
-            </div>
-        </div>
-    )
+            </div>*/
+
+        )
+    }
 }
