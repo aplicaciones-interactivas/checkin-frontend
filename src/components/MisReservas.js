cd2836375a45a3369d78dd2c82b6733723e193a6
Alex Goldstein <alex.goldstein@everis.com>
2019-11-20 20:57:51 -0300

    Updated view

--- src/components/MisReservas.js
+++ src/components/MisReservas.js
@@ -3,33 +3,41 @@ import Grid from "@material-ui/core/Grid";
 import {Card, CardContent} from "@material-ui/core";
 import HotelImage from './HotelImage';
 import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 class MisReservas extends React.Component{
     render() {
         return (
-            <Grid container justify="center">
-                <Grid item lg={10}>
+            <div>
+                <Typography variant="h2">
+                    Mis Reservas
+                </Typography>
+                <Grid container justify="center">
+                    <Grid container lg={10}>
                     <Card>
-                       <CardContent>
-                            <Grid container direction="row" spacing={3}>
-                                 <Grid item lg={4} container justify="center" alignItems="flex-end">
-                                     <HotelImage isGrid={true} images={["","",""]} />
-                                 </Grid>
-                                 <Grid item lg={4} container>
-                                     <h1>Nombre Hotel - Ciudad - País</h1>
-                                     <h2>Fecha Desde - Fecha Hasta</h2>
-                                     <h3>Tipo de Habitación - Precio</h3>
-                                 </Grid>
-                                 <Grid item lg={4}>
-                                     <Button variant="contained" color="primary">
-                                        Cancelar
-                                     </Button>
-                                 </Grid>
-                            </Grid>
-                        </CardContent>
-                    </Card>
+                        <CardContent>
+                                <Grid container direction="row" spacing={3}>
+                                    <Grid container lg={4} justify="center" alignItems="flex-end">
+                                        <HotelImage isGrid={true} images={["","",""]} />
+                                    </Grid>
+                                    <Grid container lg={4}>
+                                        <Typography variant="h4">Nombre Hotel - Ciudad - País</Typography>
+                                        <h2>Fecha Desde - Fecha Hasta</h2>
+                                        <h3>Tipo de Habitación - Precio</h3>
+                                    </Grid>
+                                    <Grid container lg={4} justify="center" alignItems="center">
+                                        <Grid item>
+                                            <Button variant="contained" color="primary">
+                                            Cancelar
+                                            </Button>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </div>
         )
     }
 }
