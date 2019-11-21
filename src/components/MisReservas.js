606bcefca43495a573fb34ebae36c44f1e2bca2b
Alex Goldstein <alex.goldstein@everis.com>
2019-11-20 21:53:03 -0300

    Function map

--- src/components/MisReservas.js
+++ src/components/MisReservas.js
@@ -21,20 +21,23 @@ class MisReservas extends React.Component {
     }
     
     render() {
+        const {reservas} = this.props;
         return (
             <div>
-                <Typography variant="h2">
+                <Typography variant="h3">
                     Mis Reservas
                 </Typography>
                 <Grid container justify="center">
                     <Grid container lg={10}>
+                reservas.map(reserva => {
+                return 
                     <Card>
                         <CardContent>
                                 <Grid container direction="row">
-                                    <Grid container lg={4} justify="center" alignItems="flex-end">
+                                    <Grid container lg={5} justify="center" alignItems="flex-end">
                                         <HotelImage isGrid={true} images={["","",""]} />
                                     </Grid>
-                                    <Grid container lg={4}>
+                                    <Grid container lg={5}>
                                         <Grid item><Typography variant="h4">Hotel - Ciudad - País</Typography></Grid>
                                         <Grid item><Typography variant="h5">Fecha Desde - Fecha Hasta</Typography></Grid>
                                         <Grid item>
@@ -42,7 +45,7 @@ class MisReservas extends React.Component {
                                             <Price price={'12345'}/>
                                         </Grid>
                                     </Grid>
-                                    <Grid container lg={4} justify="center" alignItems="center">
+                                    <Grid container lg={2} justify="center" alignItems="center">
                                         <Grid item>
                                             <Button variant="contained" color="primary">
                                             Cancelar
@@ -52,6 +55,7 @@ class MisReservas extends React.Component {
                                 </Grid>
                             </CardContent>
                         </Card>
+                        })
                     </Grid>
                 </Grid>
             </div>
